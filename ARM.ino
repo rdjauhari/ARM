6411039c55049f8831aa8ae28fe2675ff1e0274e
rdjauhari <33235727+rdjauhari@users.noreply.github.com>
2017-12-04 01:25:22 -0800

    Add files via upload
    Updated ARM with motor movements and sensor readings

--- ARM.ino
+++ ARM.ino
@@ -2,7 +2,6 @@
 #include "src/Ultrasonic/ultrasonic.h"
 #include "src/Motor/motor.h"
 
-
   // Ultrasonic Sensor Pins
   const int TRIG_PIN = 8;
   const int ECHO_PIN = 7;
@@ -16,13 +15,31 @@
   const int IN_4 = 7;
   
   // Motor Pins for Winch
+  const int EN_C = 8;
+  const int IN_5 = 9;
+  const int IN_6 = 10;
 
   // Objects
-  Actuator actuator1(EN_A, IN_1, IN_2);
-  Actuator actuator2(EN_B, IN_3, IN_4);
+  Actuator actuator1(EN_A, IN_1, IN_2); //vertical
+  Actuator actuator2(EN_B, IN_3, IN_4); //horizontal
+  Ultrasonic sensor1 (TRIG_PIN,ECHO_PIN); //top left from behind actuator
+  Ultrasonic sensor2 (); //top right
+  Ultrasonic sensor3 (); //bottom left
+  Ultrasonic sensor4 (); //bottom right
+  Ultrasonic sensor5 (); //winch distance
+  Motor motor(EN_C, IN_5, IN_6);
 
   // Variables
-  int distance;
+  int distance; //may not need
+  int distance1;
+  int distance2;
+  int distance3;
+  int distance4;
+  int distance5;
+  double timer;
+  double time_avg;
+  const double WINCH_DISTANCE_LEFT; //need to get this value
+  const double WINCH_DISTANCE_RIGHT; //need to get this value
 
 void setup() {
   
@@ -44,8 +61,88 @@ void setup() {
 }
 
 void loop() {
-  
-  actuator1.stop();
-  actuator2.stop();
+  timer = 0;
 
+  motor.moveRight();
+  if(distance5==WINCH_DISTANCE_RIGHT)
+  {
+    motor.stop();
+    actuator1.contract();
+    delay(2000);
+    actuator1.stop();
+    continue;
+  }
+  motor.moveLeft();
+  if(distance5==WINCH_DISTANCE_LEFT)
+  {
+    motor.stop();
+    actuator1.contract();
+    delay(2000);
+    actuator1.stop();
+    continue;
+  }
+  
+  distance1 = sensor1.readDistance();
+  distance2 = sensor2.readDistance();
+  distance3 = sensor3.readDistance();
+  distance4 = sensor4.readDistance();
+  distance5 = sensor5.readDistance();
+
+  if(distance2>(distance1*1.5))
+  {
+//    if(distance2==(distance4+5) && distance2==(distance4-5))
+//    {
+//      actuator1.extend();    
+//    }
+//    if(distance4>(distance2*1.5))
+//    {
+//      actuator1.contract();
+//      ++timer;
+//    }
+//    if(distance2>(distance4*1.5))
+//    {
+//      time_avg = timer/2;
+//          
+//      for(timer=0;timer<=time_avg;timer++)
+//      {
+//        actuator1.extend();
+//      }
+//    }
+    motor.moveRight();
+    if(distance1==(distance2+5) && distance1==(distance2-5))
+    {
+      motor.stop();
+      actuator2.extend();
+      //stops extending after a certain distance
+    }
+  }
+  else if(distance1>(distance2*1.5))
+  {
+    if(distance1==(distance3+5) && distance1==(distance3-5))
+//    {
+//      actuator1.extend();    
+//    }
+//    if(distance3>(distance1*1.5))
+//    {
+//      actuator1.contract();
+//      ++timer;
+//    }
+//    if(distance1>(distance3*1.5))
+//    {
+//      time_avg = timer/2;
+//          
+//      for(timer=0;timer<=time_avg;timer++)
+//      {
+//        actuator1.extend();
+//      }
+//    }
+    motor.moveLeft();
+    if(distance1==(distance2+5) && distance1==(distance2-5))
+    {
+      motor.stop();
+      actuator2.extend();
+      //stops extending after a certain distance
+    }
+  }
+  //reset
 }
